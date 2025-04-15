371342e3f4592a3129923d07f262e03f85d19e21
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2025-04-14 23:20:17 -0500

    feat: login page implemented
    Persists the authenticated user ID in session

--- Group7_iFINANCEAPP/App_Start/RouteConfig.cs
+++ Group7_iFINANCEAPP/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace Group7_iFINANCEAPP
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "Login"
+                );
         }
     }
 }
--- Group7_iFINANCEAPP/Controllers/LoginController.cs
+++ Group7_iFINANCEAPP/Controllers/LoginController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Group7_iFINANCEAPP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,12 +11,38 @@ namespace Group7_iFINANCEAPP.Controllers
 {
     public class LoginController : Controller
     {
+        private Group7_iFINANCEDBEntities db = new Group7_iFINANCEDBEntities();
+
         // GET: Login
-        public ActionResult LoginPage()
+        public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string username, string password)
+        {
+            byte[] pass = UTF8Encoding.UTF8.GetBytes(Request.Form["password"]);
+            byte[] hash = SHA256.Create().ComputeHash(pass);
+
+            string encryptedPassword = NonAdminUsersController.GetHashString(hash);
+
+            UserPassword p = db.UserPassword.Where(a => a.userName == username).Where(a => a.encryptedPassword == encryptedPassword).FirstOrDefault();
+
+            if (p != null)
+            {
+                Session["NonAdminUserID"] = p.NonAdminUserID;
+                Session["AdministratorID"] = p.AdministratorID;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ValidationError = "Invalid username or password.";
+                return View();
+            }
+
+        }
  
     }
 }
