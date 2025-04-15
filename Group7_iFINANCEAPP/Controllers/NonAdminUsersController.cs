3dfcb346e08b1e5b55c9c13688767d06b7f5f452
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2025-04-14 21:10:45 -0500

    fix: auto-incremement implemeneted

--- Group7_iFINANCEAPP/Controllers/NonAdminUsersController.cs
+++ Group7_iFINANCEAPP/Controllers/NonAdminUsersController.cs
@@ -48,7 +48,7 @@ namespace Group7_iFINANCEAPP.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,name,address,email,AdministratorID")] NonAdminUser nonAdminUser)
+        public ActionResult Create([Bind(Include = "name,address,email,AdministratorID")] NonAdminUser nonAdminUser)
         {
             if (ModelState.IsValid)
             {
@@ -82,7 +82,7 @@ namespace Group7_iFINANCEAPP.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,name,address,email,AdministratorID")] NonAdminUser nonAdminUser)
+        public ActionResult Edit([Bind(Include = "name,address,email,AdministratorID")] NonAdminUser nonAdminUser)
         {
             if (ModelState.IsValid)
             {
