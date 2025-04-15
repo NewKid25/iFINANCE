a9d6da3f9b12a427d9448bd845b815a00a83fd7a
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2025-04-14 21:36:37 -0500

    added home page and stylings

--- Group7_iFINANCEAPP/Controllers/ChartAccountsController.cs
+++ Group7_iFINANCEAPP/Controllers/ChartAccountsController.cs
@@ -46,7 +46,7 @@ namespace Group7_iFINANCEAPP.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,name,type")] AccountCategory accountCategory)
+        public ActionResult Create([Bind(Include = "name,type")] AccountCategory accountCategory)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace Group7_iFINANCEAPP.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,name,type")] AccountCategory accountCategory)
+        public ActionResult Edit([Bind(Include = "name,type")] AccountCategory accountCategory)
         {
             if (ModelState.IsValid)
             {
