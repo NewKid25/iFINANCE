3a2674bb99a8d204540099113e9e94d47a7f91fd
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2025-04-21 19:48:17 -0500

    feat: report stubs

--- Group7_iFINANCEAPP/Controllers/ReportsController.cs
+++ Group7_iFINANCEAPP/Controllers/ReportsController.cs
@@ -8,15 +8,36 @@ namespace Group7_iFINANCEAPP.Controllers
 {
     public class ReportsController : Controller
     {
+
+        public int[] junk = new int[] { 1, 2, 3 };
         // GET: Reports
-        public ActionResult ReportsPage()
+        public ActionResult Index()
         {
             if (Session["NonAdminUserID"] == null)
             {
                 return RedirectToAction("Index", "Login");
             }
 
+            return View(junk);
+        }
+
+        public ActionResult ProfitLossReport()
+        {
+            return View(junk);
+        }
+
+        public ActionResult TrialBalance()
+        {
             return View();
         }
+    }
+
+   
+
+    public enum ReportType
+    {
+        NONE,
+        TRIALBALANCE,
+        PROFITLOSS
     }
 }
