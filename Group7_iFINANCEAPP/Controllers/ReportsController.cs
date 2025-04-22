cc7a3412215febf80641ca74c6586d5261073902
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2025-04-21 22:15:36 -0500

    feat: trail balance
    - everything the totals

--- Group7_iFINANCEAPP/Controllers/ReportsController.cs
+++ Group7_iFINANCEAPP/Controllers/ReportsController.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Group7_iFINANCEAPP.Models;
+using Group7_iFINANCEAPP.Models.ViewModels;
 
 namespace Group7_iFINANCEAPP.Controllers
 {
     public class ReportsController : Controller
     {
+        private Group7_iFINANCEDBEntities db = new Group7_iFINANCEDBEntities();
 
-        public int[] junk = new int[] { 1, 2, 3 };
+
         // GET: Reports
         public ActionResult Index()
         {
@@ -18,26 +24,68 @@ namespace Group7_iFINANCEAPP.Controllers
                 return RedirectToAction("Index", "Login");
             }
 
-            return View(junk);
+            return View();
         }
 
-        public ActionResult ProfitLossReport()
+        public ActionResult ProfitLoss()
         {
-            return View(junk);
+            return View();
         }
 
         public ActionResult TrialBalance()
         {
-            return View();
+            int nonAdminUserID = (int)Session["NonAdminUserID"];
+
+            var masterAccounts = db.MasterAccount.Where(a => a.Group.NonAdminUserID == nonAdminUserID).ToList();
+
+            var trialBalanceLineItems = new List<TrailBalanceLineItem>();
+            foreach(var account in masterAccounts)
+            {
+                var lineItem = new TrailBalanceLineItem(account.name);
+                if(account.Group.AccountCategory.type == "debit")
+                {
+                    lineItem.debit = account.closingAmount;
+                }
+                else
+                {
+                    lineItem.credit = account.closingAmount;
+                }
+                trialBalanceLineItems.Add(lineItem);
+                
+            }
+
+            //ViewBag.trialBalanceLineItems = trialBalanceLineItems;
+            var totalDebit = trialBalanceLineItems.Sum(item => item.debit);
+            var totalCredit = trialBalanceLineItems.Sum(item => item.credit);
+
+            //masterAccounts[0].Group.AccountCategory.type
+
+            //    if(Type == debit)
+            //    debit = totalClosingAmount
+            //    else
+            //    credit = totalClosingAmount
+
+            return View(new TrialBalanceViewModel(trialBalanceLineItems,totalDebit,totalCredit));
         }
     }
 
-   
+
 
     public enum ReportType
     {
         NONE,
         TRIALBALANCE,
         PROFITLOSS
+    }
+
+    public class TrailBalanceLineItem
+    {
+        public TrailBalanceLineItem(string accountName)
+        {
+            this.accountName = accountName;
+        }
+        public string accountName;
+        public double? debit { get; set; }
+        public double? credit { get; set; }
     }
 }
---
+++ Group7_iFINANCEAPP/Models/ViewModels/TrialBalanceViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Group7_iFINANCEAPP.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group7_iFINANCEAPP.Models.ViewModels
+{
+    public class TrialBalanceViewModel
+    {
+        public TrialBalanceViewModel(List<TrailBalanceLineItem> lineItems, double? totalDebit, double? totalCredit)
+        {
+            this.lineItems = lineItems;
+            this.totalDebit = (double)totalDebit;
+            this.totalCredit = (double)totalCredit;
+        }
+
+        public List<TrailBalanceLineItem> lineItems { get; set; }
+        public double totalDebit { get; set; }
+        public double totalCredit { get; set; }
+    }
+}
